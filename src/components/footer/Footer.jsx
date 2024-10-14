faa301ead45eee92fb072427279f55860f910b62
alikhanov <alixonovshukurullo22@gmail.com>
2024-10-14 18:25:27 +0500

    138

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./footer.css";
+import document from "../../assets/photo_2023-12-14_18-38-55.jpg";
 
 const Footer = () => {
      return (
           <footer className='footer__container'>
                <div className="footer__section">
-                    <div className="footer__company-info">
-                         <h4>О компании</h4>
-                         <p>ALIKHANOV DEV занимается разработкой веб-решений и продажей продуктов. Мы стремимся предоставлять качественные сервисы для наших клиентов.</p>
-                    </div>
-               </div>
-               <div className="footer__section">
                     <div className="footer__links">
                          <h4>Полезные ссылки</h4>
                          <Link to="/">Главная</Link>
                          <Link to="/catalog">Каталог продуктов</Link>
-                         <Link to="/terms-of-service">Условия использования</Link> {/* Добавлена ссылка на Условия использования */}
-                         <Link to="/refund-policy">Политика возврата</Link> {/* Добавлена ссылка на Политику возврата */}
+                         <Link to="/terms-of-service">Условия использования</Link>
+                         <Link to="/refund-policy">Политика возврата</Link>
                     </div>
                </div>
                <div className="footer__section">
@@ -25,12 +20,20 @@ const Footer = () => {
                          <h4>Контакты</h4>
                          <p>Телефон: +99894 075 13 13</p>
                          <p>Email: alixonovshukurullo13@gmail.com</p>
-                         {/* <Link to="https://www.alikhanov.uz" target="_blank" rel="noopener noreferrer">Автор</Link> */}
-
                     </div>
                     <div className="footer__social-media">
-                         <Link to="https://t.me/alikhanov13">Telegram</Link>
-                         <Link to="https://www.instagram.com/alikhanov.13/">Instagram</Link>
+                         <a href="https://t.me/alikhanov13">Telegram</a>
+                         <a href="https://www.instagram.com/alikhanov.13/">Instagram</a>
+                    </div>
+               </div>
+               <div className="footer__section">
+                    <div className="footer__company-info">
+                         <h4>О компании</h4>
+                         <p>ООО ALIXANOV-IT GROUP — продажа музыкальных инструментов для всех уровней музыкантов. Качество, надежность, удобство. Широкий ассортимент и доступные условия покупки.</p>
+                       
+                         <a href={document} download="photo_2023-12-14_18-38-55.jpg" className="download-button">
+                              Скачать документ
+                         </a>
                     </div>
                </div>
           </footer>
